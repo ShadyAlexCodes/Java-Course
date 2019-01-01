07a268fdf12713d1787a5267edb43e3ee0e607ef
Alex <shadyalexcodes@gmail.com>
2019-01-01 14:07:08 -0800

    Char and Booleans
    Features how Characters and Booleans work, as well as a challenge finding the registered symbol and printing it.

---
+++ projectFour/src/io/github/shadyalexcodes/ChallengeFour.java
@@ -0,0 +1,23 @@
+package io.github.shadyalexcodes;
+
+public class ChallengeFour {
+
+    // Shortcut is psvm
+
+    public static void main(String[] args) {
+
+        /*
+
+            1. Find the code for the registered symbol on the same line as the copyright symbol.
+            2. Create a variable of type char and assign it to he unicode value for that symbol.
+            3. Display this variable on screen,
+
+         */
+
+        char registeredSymbol = '\u00AE';
+        System.out.println("Registered Symbol: " + registeredSymbol);
+
+
+
+    }
+}
---
+++ projectFour/src/io/github/shadyalexcodes/ProjectFour.java
@@ -0,0 +1,14 @@
+package io.github.shadyalexcodes;
+
+public class ProjectFour {
+
+    public static void main(String[] args) {
+	    char myChar = '\u00A4'; // Char's can only store one character
+        System.out.println("Unicode character output: " + myChar); // Creates the Copy Right Symbol ¤
+
+        boolean myBoolean = false; // Booleans are often used for conditional statements, and can only be true of false.
+        boolean isHuman = true;
+
+    }
+
+}
