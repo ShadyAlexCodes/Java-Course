236f51f81eb86fcabc5a0fddc7adcd05bb2c807f
Alex <shadyalexcodes@gmail.com>
2018-12-31 22:37:16 -0800

    Introductions
    This update features the creation and concept of System.out.println to this project, and my understanding of it and how it works.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Java-Course
+# Java Course Work
+
+This repository will contain any projects created for Java through my classes or for fun. In each update I will do my best to provide as much detailed information about a topic as possible.  
+
+
+### Extra
+[License](http://github.com)
+
+[Wiki](http://github.com)
--- projectOne/src/io/github/shadyalexcodes/Main.java
+++ projectOne/src/io/github/shadyalexcodes/Main.java
@@ -1,4 +1,20 @@
 package io.github.shadyalexcodes;
 
 public class Main {
+
+    public static void main(String[] args) {
+        /*
+
+        What am I doing?
+
+            While System.out.println is a Java statement that is used to print an
+            argument that has been passed through the System.out statement. The word `System.`
+            is a final class created in java.lang package. The `.out` is a static member of the System class
+            and is a PrintSteam. 'println' is a method of the PrintSteam class, in which prints any given argument based
+            onto a new line. Each time that these arguments are placed together and make a command which prints the line "Hello World!"
+
+        */
+        System.out.println("Hello World!!");
+    }
+
 }
